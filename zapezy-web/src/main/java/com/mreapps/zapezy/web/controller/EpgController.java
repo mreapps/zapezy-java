797eea971ce7062192a80ce5b3c63971726c0312
Espen Simensen <espen.simensen@broadpark.no>
2013-07-17 23:14:11 +0200

    i18n on epg update

--- zapezy-web/src/main/java/com/mreapps/zapezy/web/controller/EpgController.java
+++ zapezy-web/src/main/java/com/mreapps/zapezy/web/controller/EpgController.java
@@ -1,17 +1,22 @@
 package com.mreapps.zapezy.web.controller;
 
+import com.mreapps.zapezy.service.entity.StatusMessage;
+import com.mreapps.zapezy.service.entity.StatusMessageType;
 import com.mreapps.zapezy.service.service.EpgService;
+import com.mreapps.zapezy.service.service.MessageSourceService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Locale;
 
 @Controller
 public class EpgController
@@ -21,29 +26,33 @@ public class EpgController
     @Autowired
     private EpgService epgService;
 
+    @Autowired
+    private MessageSourceService messageSourceService;
+
+    @Secured({"ROLE_ADMIN"})
     @RequestMapping(value = "/admin/epg/refreshProgrammes", method = RequestMethod.GET)
-    public String refreshProgrammes(ModelMap model)
+    public String refreshProgrammes(Locale locale, RedirectAttributes redirectAttributes)
     {
         long start = System.currentTimeMillis();
-        String statusMessage;
+        StatusMessage statusMessage;
         try
         {
             int programmeCount = epgService.refreshEpg();
             double timeInSeconds = BigDecimal.valueOf(System.currentTimeMillis()).subtract(BigDecimal.valueOf(start)).divide(BigDecimal.valueOf(1000), 3, RoundingMode.HALF_UP).doubleValue();
 
-            statusMessage = "Refreshed " + programmeCount + " programmes in " + timeInSeconds + " seconds.";
+            statusMessage = new StatusMessage(StatusMessageType.SUCCESS, messageSourceService.get("refreshed_x1_programmes_in_x2_seconds", locale, programmeCount, timeInSeconds));
         } catch (IOException e)
         {
             logger.error(e.getMessage(), e);
-            statusMessage = "Channel update failed: " + e.getMessage();
+            statusMessage = new StatusMessage(StatusMessageType.ERROR, messageSourceService.get("epg_update_failed", locale, e.getMessage()));
         } catch (JAXBException e)
         {
             logger.error(e.getMessage(), e);
-            statusMessage = "Channel update failed: " + e.getMessage();
+            statusMessage = new StatusMessage(StatusMessageType.ERROR, messageSourceService.get("epg_update_failed", locale, e.getMessage()));
         }
 
-//        StatusMessageModel.setInfoMessage(model, statusMessage);
+        redirectAttributes.addFlashAttribute("STATUS_MESSAGE", statusMessage);
 
-        return "home.jsp";
+        return "redirect:/";
     }
 }
